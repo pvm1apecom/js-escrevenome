fcf07e1e8e43344ec0afb1425ec16690a2dbb6c7
pvm1apecom <168188350+pvm1apecom@users.noreply.github.com>
2024-07-02 07:55:46 -0300

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,1 +1,17 @@
+function setup() {
+  createCanvas(600, 600);
+  background("red");
+}
 
+function draw() {
+  
+
+  stroke("purple");
+  fill("yellow");
+
+  // console.log(mouseIsPressed)
+
+  if (mouseIsPressed) {
+    rect(mouseX, mouseY, 20, 45);
+  }
+}
